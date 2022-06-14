0506f1bb75da5ba45d9d10060244dd281d5c2d87
karishma <karishmakumari724789@gmail.com>
2022-06-14 18:26:43 +0530

    bind city list xml with city list data binding

--- app/src/main/java/com/example/androidktxproject/uilayer/CityListAdapter.kt
+++ app/src/main/java/com/example/androidktxproject/uilayer/CityListAdapter.kt
@@ -4,13 +4,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.library.baseAdapters.BR
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidktxproject.R
+import com.example.androidktxproject.databinding.CityListRawBinding
 import kotlinx.android.synthetic.main.city_list_raw.view.*
 
 class CityListAdapter : RecyclerView.Adapter<CityListAdapter.ViewHolder>() {
     var cityList: List<String>? = null
-
+    private lateinit var binding: CityListRawBinding
     fun getCityListData(mCityList: List<String> = emptyList()) {
         this.cityList = mCityList
         notifyDataSetChanged()
@@ -18,16 +20,17 @@ class CityListAdapter : RecyclerView.Adapter<CityListAdapter.ViewHolder>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.city_list_raw, parent, false)
-        return ViewHolder(view)
+        binding = CityListRawBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+//        val view =
+//            LayoutInflater.from(parent.context).inflate(R.layout.city_list_raw, parent, false)
+        return ViewHolder(binding)
     }
 
 
     // binds the list items to a view
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         // sets the text to the textview from our itemHolder class
-        holder.textView.text = cityList?.get(position) ?: ""
-
+        holder.bind(cityList?.get(position) ?: "")
     }
 
 
@@ -37,8 +40,10 @@ class CityListAdapter : RecyclerView.Adapter<CityListAdapter.ViewHolder>() {
 
 
     // Holds the views for adding it to image and text
-    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
-        val textView: TextView = itemView.findViewById(R.id.textView)
+    class ViewHolder(var binding: CityListRawBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(cityName: String) {
+            binding.setVariable(BR.cityName,cityName)
+        }
     }
 
 }
